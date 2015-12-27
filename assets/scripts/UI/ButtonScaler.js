ae1fa014c6c1575c45f57cfc26fe347dd4d3bda8
Nan Wang <nantas@gmail.com>
2015-12-27 17:50:50 +0800

    add button scaler helper component.

---
+++ assets/scripts/UI/ButtonScaler.js
@@ -0,0 +1,27 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        pressedScale: 1,
+        transDuration: 0
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        var self = this;
+        self.initScale = this.node.scale;
+        self.button = self.getComponent(cc.Button);
+        self.scaleDownAction = cc.scaleTo(self.transDuration, self.pressedScale);
+        self.scaleUpAction = cc.scaleTo(self.transDuration, self.initScale);
+        function onTouchDown (event) {
+            this.stopAllActions();
+            this.runAction(self.scaleDownAction);
+        }
+        function onTouchUp (event) {
+            this.stopAllActions();
+            this.runAction(self.scaleUpAction);
+        }
+        this.button.on(cc.Button.EVENT_TOUCH_DOWN, onTouchDown, this.node);
+        this.button.on(cc.Button.EVENT_TOUCH_UP, onTouchUp, this.node);
+    },
+});
