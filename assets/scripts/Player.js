b4ce4e84ce9a5a3853599d429d5e5365d781a4b8
Nan Wang <nantas@gmail.com>
2015-12-28 10:24:24 +0800

    update audiomng.

---
+++ assets/scripts/Player.js
@@ -0,0 +1,28 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        playerInfo: {
+            default: null,
+            type: cc.Node
+        },
+        stakeOnTable: {
+            default: null,
+            type: cc.Node
+        },
+        cardInfo: {
+            default: null,
+            type: cc.Node
+        }
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame
+    update: function (dt) {
+
+    },
+});
